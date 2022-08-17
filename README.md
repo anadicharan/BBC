92a6e0f7b70fb4991fefad3d651ad1cf86c97316
Hati <anadi.hati@capgemini.com>
2022-08-17 16:33:26 +0530

    adding files

---
+++ README.md
@@ -0,0 +1,57 @@
+# BBC-CLONE 
+## We have cloned BBC which is world wide popular news and information provider website. 
+## Tech stack 
+* REACT 
+* JAVA SCRIPT
+* CSS  
+    ## Other libraries
+    * redux 
+    * react-redux
+    * redux-thunk
+    * react-loading-skeleton 
+    * react-toastify 
+    * react-awesome-slider 
+    * react-youtube 
+    * react-player 
+    * react-social
+    * react-icons
+    * axios
+    * uuid
+
+## How to use the product
+1. By clicking this [link](https://bbc-clone-methane.netlify.app/) User will be redirected to Home Page.
+![<HomePage>](https://tgdown.eu-gb.mybluemix.net/7805521439850432/2021-03-15_10:10:21.jpg)
+
+2. From Navbar any person can login as Normal user or as Admin if they already has an account, or they can register.
+![<LoginPage>](<https://tgdown.eu-gb.mybluemix.net/7805555799588800/2021-03-15_10:10:35.jpg>)
+
+3. If user is Admin he can Post Stories from Accounts page.
+![<LoginPage>](https://tgdown.eu-gb.mybluemix.net/7806895829385152/2021-03-15_10:38:16.jpg)
+
+4. Posted Stories will be displayed as Latest Story in News Page.
+![<NewsPage>](https://tgdown.eu-gb.mybluemix.net/7805624519065536/2021-03-15_10:13:19.jpg)
+
+5. User can type any query and search related news from search bar.
+![<SearchBar>](https://tgdown.eu-gb.mybluemix.net/7805693238542272/2021-03-15_10:13:51.jpg)
+
+6. By clicking any news story user will be redirected to single article page where he can read whole article.
+![<SinglePage>](https://tgdown.eu-gb.mybluemix.net/7805727598280640/2021-03-15_10:13:56.jpg)
+
+***
+
+## Team Members and Contributors
+
+### Priyanka Poshala
+- **Github** : https://github.com/priyanka-890
+
+### Pranit Jogwe
+- **Github** : https://github.com/pranit95
+
+### Akash Vaghela
+- **Github** : https://github.com/akashvaghela09
+
+### Abhinav Raj
+- **Github** : https://github.com/AbhinavRajSing
+
+### Bhairavesware Reddy
+- **Github** : https://github.com/BhairavReddy67
---
+++ src/Component/Navbar/Navbar.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "../Styles/navbar.module.css"
+import SearchBar from "../Search/SearchBar";
+import { RiArrowDropDownFill } from "react-icons/ri";
+import Dropdown from "./Dropdown";
+import { useSelector } from "react-redux";
+
+export default function Navbar() {
+const {data_auth,isAuth}=useSelector(state=>state.login)
+
+console.log(data_auth,'333');
+const linkLeft = [
+  {
+    to: "/",
+    title: "",
+  },
+  {
+    to: !isAuth?"/Sign":"/Account",
+    title:!isAuth? "Sign in":data_auth[0].category==="normal"?`${data_auth[0].name} *`:`${data_auth[0].name}`,
+  },
+];
+
+const linkMiddle = [
+  {
+    to: "/",
+    title: "Home",
+  },
+  {
+    to: "/news",
+    title: "News",
+  },
+  {
+    to: "/sport",
+    title: "Sport",
+  },
+  {
+    to: "/reel",
+    title: "Reel",
+  },
+  {
+    to: "/worklife",
+    title: "Worklife",
+  },
+  {
+    to: "/travel",
+    title: "Travel",
+  },
+  {
+    to: "/future",
+    title: "Future",
+  },
+  {
+    to: "/culture",
+    title: "Culture",
+  },
+  {
+    to: "/more",
+    title: "More",
+  },
+];
+
+  const [moreDrop, setMoreDrop] = React.useState(false);
+  const showDropDown=()=>{
+    setMoreDrop(!moreDrop)
+  }
+  return (
+    <div>
+      <div className={style.navBar__wrap}>
+        <div>
+          {linkLeft?.map(({ to, title }, index) => (
+            <Link key={title} className={style.navBar__left} to={to}>
+              {index===0 && <img className={style.bbclogo} src="/bbclogo.png" alt=""/>}
+              {title}
+            </Link>
+          ))}
+        </div>
+        <div>
+          {linkMiddle?.map(({ to, title }, index) => (
+            <Link key={title} className={style.navBar__right} to={to}>
+              {title}
+            </Link>
+          ))}
+        </div>
+        <div onClick={showDropDown} className={style.dropdown__icon}>
+          <RiArrowDropDownFill />
+        </div>
+        <SearchBar></SearchBar>
+      </div>
+      {moreDrop && <Dropdown></Dropdown> }
+    </div>
+  );
+}
